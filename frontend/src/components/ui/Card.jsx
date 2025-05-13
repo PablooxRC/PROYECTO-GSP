80721e0ba4cdb96de50eb48ff1087efb14fcab5f
Tu Nombre <pabloox73@gmail.com.com>
2025-05-13 01:16:44 -0400

    Cambios front 13 de mayo

---
+++ frontend/src/components/ui/Card.jsx
@@ -0,0 +1,5 @@
+export const Card = ({ children }) => (
+  <div className="bg-gray-600 p-6 rounded-lg shadow-lg">
+    {children}
+  </div>
+);
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import {Button, Input, Card} from "../components/ui/Index.js"
+import {useForm} from 'react-hook-form'
 function RegisterPage(){
+
+  const {register, handleSubmit} = useForm()
+
+  const onSubmit = handleSubmit(data =>{
+    console.log(data)
+  })
   return (
-   <div>Home Page</div>
+    <div className='h-[calc(100vh-64px)] flex items-center justify-center'>
+      <Card>
+        <h3 className='text-2xl font-bold'> Register</h3>
+        <form onSubmit={onSubmit}>
+          <Input placeholder="Ingresa tu CI"
+            {... register('ci',{
+              required: true,
+            })}
+          />
+          <Input placeholder="Ingresa tu nombre"
+            {... register('nombre', {
+              required: true,
+            })}
+          />
+          <Input placeholder="Ingresa tu apellido"
+            {... register('apellido', {
+              required: true,
+            })}
+          />
+          <Input type ="email" placeholder="Ingresa tu email"
+            {... register('email', {
+              required: true,
+            })}
+          />
+          <Input placeholder="Ingresa tu unidad"
+            {... register('unidad', {
+              required: true,
+            })}
+          />
+          <Input type ="password" placeholder="Ingresa tu contraseña"
+            {... register('password', {
+              required: true,
+            })}
+          />
+          <Button>
+            Registrar
+          </Button>
+        </form>
+      </Card>
+    </div>
   )
 }
 
